8829ddf7c4333795b982beb1d073092f975dfd6f
JoMingyu <city7310@naver.com>
2017-04-01 17:43:55 +0900

    푸쉬알림 기능 구현 완료

--- firebase/firebase_push.py
+++ firebase/firebase_push.py
@@ -1,26 +1,18 @@
 from pyfcm import FCMNotification
 from database import Database
+import query_formats
 
 
 class FCMSender:
-
-    _instance = None
-
-    def __new__(cls):
-        if not cls._instance:
-            cls._instance = object.__new__(cls)
-
-        return cls._instance
-
     def __init__(self, server_key):
         self.push_service = FCMNotification(api_key=server_key)
         self.db = Database()
 
-    def send(self):
+    def send(self, title, message):
         registration_ids = list() # DB에서 가져오는 클라이언트 id들
         clients_to_push = self.get_clients_to_push(registration_ids, self.db)
-        message_title = 'HowMuch? 실시간 환율정보 알리미'
-        message_body = '환율 변동 정보가 있습니다!'
+        message_title = title
+        message_body = message
 
         result = self.push_service.notify_multiple_devices(registration_ids=clients_to_push,
                                                            message_title=message_title,
@@ -30,11 +22,13 @@ class FCMSender:
 
     @staticmethod
     def get_clients_to_push(registration_ids, db):
-        clients_to_push = list() # DB에서 옵션 확인 후 푸쉬알림을 줄 클라이언트 id 리스트
+        rows = db.execute(query_formats.get_registration_id_format)
+        clients_to_push = list()
+        for row in rows:
+            clients_to_push.append(row['registration_key'])
 
-        for registration_id in registration_ids:
-            db.execute()
-            # if ~~:
-            #     clients_to_push.append(registration_id)
-
+        print(clients_to_push)
         return clients_to_push
+
+sender = FCMSender('AAAA9LkX9Zs:APA91bHyu886FvO46OzqQLkBofJfxpszMI9_HpBrMTPkTed46hzsColH0K9sloVNRaiRwszAn17eDPfue5-KtzS0Q0gPekTgyzRa1CjgbpBathxpD6OH0lsj8CHDJVt8DDRQuVu8_Xjh')
+sender.send('푸쉬푸쉬', '히히히')
